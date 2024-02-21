e6b1eaca33c3cd5c28d830ef3e25c5de9ec16174
ParabolicNet <parabolicweb@protonmail.com>
2024-02-21 15:44:08 +1300

    Update EmailFom.tsx

--- components/EmailFom.tsx
+++ components/EmailFom.tsx
@@ -53,7 +53,7 @@ export default function EmailForm() {
             onChange={handleEmailChange}
           />
           <button
-            className="flex h-10 shrink-0 items-center justify-center gap-1 rounded-lg bg-[#FED545] px-4 py-2 text-sm font-semibold text-gray-800 transition-all hover:bg-yellow-600"
+            className="flex h-10 shrink-0 items-center justify-center gap-1 rounded-lg bg-[#FED545] px-4 py-2 text-sm font-semibold text-gray-800 transition-all hover:bg-yellow-500"
             type="submit"
           >
             <span>Join the waitlist</span>
