d3af9d7c2e9886d0b276a80f504f3e2265ffae15
ParabolicNet <parabolicweb@protonmail.com>
2024-02-21 14:53:04 +1300

    Image, page updates

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="md:h-full h-80 bg-[#C9F9D1] relative overflow-hidden">
           <Image
-            src="/mobile-app.png"
-            alt="Mobile App Screenshots"
+            src="/CreateRealityWBG.webp"
+            alt="Create Reality Image"
             fill
             className="object-contain mt-8 md:mt-24 px-14 object-bottom"
           />
