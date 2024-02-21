94cdf3f5a12b4138464fbc1171bcb201accbbecd
ParabolicNet <parabolicweb@protonmail.com>
2024-02-21 15:27:28 +1300

    Update image crop for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
     <>
       <Toaster />
 
-      <section className="w-screen h-screen grid grid-cols-1 md:grid-cols-2 gap-6 bg-[#121212]">
-        <div className="md:h-full bg-[#121212] relative overflow-hidden">
+      <section className="w-screen h-screen grid md:grid-cols-2 gap-6 bg-[#121212]">
+        <div className="md:h-full h-1/2 bg-[#121212] relative overflow-hidden md:order-1 order-2">
           <div 
             style={{
               backgroundImage: `url('/CreateRealityWBG.webp')`,
@@ -21,7 +21,7 @@ export default function Home() {
           ></div>
         </div>
 
-        <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10">
+        <main className="flex flex-col gap-8 mt-8 justify-center px-6 pb-10 pl-6 md:order-2 order-1">
           <h1 className="font-semibold tracking-tight bg-[#FED545] text-3xl leading-tight md:text-4xl max-w-lg">
             Live a truly great life - join the waitlist now
           </h1>
